c475287f3234b592c8f812e8d23812c0d4ead319
seepine <seepine@163.com>
2020-06-15 11:00:32 +0800

    remove annotation

--- packages/index.js
+++ packages/index.js
@@ -9,7 +9,6 @@ import ClTimePicker from './time-picker'
 import ClDatePicker from './date-picker'
 import ClMenu from './menu'
 
-
 const components = [
     ClForm, ClTable, ClInput, ClRadio, ClCheckBox, ClSelect, ClInputNumber, ClMenu, ClTimePicker, ClDatePicker
 ];
@@ -18,15 +17,7 @@ const install = function (Vue) {
     components.forEach(component => {
         Vue.component(component.name, component);
     });
-    //挂载
-    // Vue.prototype.$message = Message;
-
 };
-
-/* istanbul ignore if */
-if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue);
-}
 
 export default {
     install,
