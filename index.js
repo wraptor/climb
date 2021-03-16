4ec6d5b3dc9c539984b2bb17849ad3cbb68b9c2a
wraptor <wraptor@163.com>
2021-03-16 14:44:51 +0800

    fix install and add date.format

--- index.js
+++ index.js
@@ -20,24 +20,56 @@ const components = [
   ClDatePicker
 ];
 
-const install = function(Vue) {
-  components.forEach(component => {
-    Vue.component(component.name, component);
-  });
-};
-/* istanbul ignore if */
-if (typeof window !== "undefined" && window.Vue) {
-  install(window.Vue);
-}
+(function() {
+  // Defining patterns
+  var dateFormat = function(date, format = "yyyy-MM-dd hh:mm:ss") {
+    if (date) {
+      if (Object.prototype.toString.call(date) !== "[object Date]") {
+        return "";
+      }
+      const o = {
+        "M+": date.getMonth() + 1, // month
+        "d+": date.getDate(), // day
+        "D+": date.getDate(), // day
+        "h+": date.getHours(), // hour
+        "H+": date.getHours(), // hour
+        "m+": date.getMinutes(), // minute
+        "s+": date.getSeconds(), // second
+        "q+": Math.floor((date.getMonth() + 3) / 3), // quarter
+        S: date.getMilliseconds() // millisecond
+      };
+      if (/(y+)/.test(format) || /(Y+)/.test(format)) {
+        format = format.replace(
+          RegExp.$1,
+          (date.getFullYear() + "").substr(4 - RegExp.$1.length)
+        );
+      }
+      for (const k in o) {
+        if (new RegExp("(" + k + ")").test(format)) {
+          format = format.replace(
+            RegExp.$1,
+            RegExp.$1.length === 1
+              ? o[k]
+              : ("00" + o[k]).substr(("" + o[k]).length)
+          );
+        }
+      }
+      return format;
+    }
+    return "";
+  };
+
+  // Simulates PHP's date function
+  Date.prototype.format = function(format = "yyyy-MM-dd hh:mm:ss") {
+    return dateFormat(this, format);
+  };
+}).call(this);
+
+
 export default {
-  install,
-  // ClForm,
-  // ClTable,
-  // ClInput,
-  // ClRadio,
-  // ClCheckBox,
-  // ClSelect,
-  // ClMenu,
-  // ClTimePicker,
-  // ClDatePicker
+  install(Vue) {
+    components.forEach(component => {
+      Vue.component(component.name, component);
+    });
+  }
 };
