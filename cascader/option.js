f71bb6219f5349cb68e64c26f9362c02ecf50966
wraptor <wraptor@163.com>
2021-05-19 10:24:38 +0800

    add cascader

---
+++ cascader/option.js
@@ -0,0 +1,14 @@
+export default {
+  //element attributes
+  // clearable: true,
+
+  //ext
+  dicData: [], //字典数据
+  props: {
+    //字典的prop修改
+    label: "label",
+    value: "value",
+    children: "children",
+    data: "data"
+  }
+};
